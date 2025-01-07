47bce2d94e8e0c4306802f8b50b55086e7a84504
Karthik B <karthikb261993@gmail.com>
2025-01-07 22:45:12 +0530

    second commit

--- tests/magentoLogin.spec.js
+++ tests/magentoLogin.spec.js
@@ -1,10 +1,10 @@
 import { test } from '@playwright/test';
 import cred from '../cred/creds.json';
-import MegnatoPage from '../pages/megnatoLogin.page';
+import MagentoPage from '../pages/magentoLogin.page';
 
 test.describe('Verifing Login with valid and invalid credentials', async () => {
     test("Megnato valid Cred", async ({ page }) => {
-        let signinPage = new MegnatoPage(page);
+        let signinPage = new MagentoPage(page);
         await signinPage.navigateToUrl(cred.url);
         await signinPage.clickSigninFromHome();
         await signinPage.signinAsUser(cred.valid.username, cred.valid.password);
